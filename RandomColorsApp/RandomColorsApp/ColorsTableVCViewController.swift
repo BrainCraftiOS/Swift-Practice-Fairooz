e3c1c72f1e6fc29460e4a66e0c7fda2f898172c2
Fairooz Rahman <fairooz@braincraftapps.com>
2023-08-17 19:41:00 +0600

    check

---
+++ RandomColorsApp/RandomColorsApp/ColorsTableVCViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ColorsTableVCViewController.swift
+//  RandomColorsApp
+//
+//  Created by Fairooz Rahman on 17/8/23.
+//
+
+import UIKit
+
+class ColorsTableVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
