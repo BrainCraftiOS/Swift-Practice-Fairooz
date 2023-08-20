112b14ca5b20d22718c154445a767b7f5fd332e1
Fairooz Rahman <fairooz@braincraftapps.com>
2023-08-20 19:48:27 +0600

    ATD-747 # Creating and combining views in SwiftUI

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Fairooz Rahman on 20/8/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
